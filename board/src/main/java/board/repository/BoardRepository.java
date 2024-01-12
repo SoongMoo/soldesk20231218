0233cfe27f923fdf8dc571333a646b08b82d178c
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2024-01-12 15:21:26 +0900

    Delete board directory

--- board/src/main/java/board/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package board.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import board.domain.BoardDTO;
-
-public interface BoardRepository  extends MongoRepository<BoardDTO, String>{
-	void deleteById(String id);
-}
